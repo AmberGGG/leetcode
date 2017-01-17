d1adc6f68f44fc0ed797f7970683742bce6a099e
AmberGGG <jieyao.gao@gmail.com>
2017-01-17 10:53:33 -0500

    add first binary search codes

--- 14 first position binary search.java
+++ 14 first position binary search.java
@@ -1,10 +1,28 @@
 package cs61b;
 
 public class Solution {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static int binarySearch(int[] nums, int target){
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int start = 0;
+        int end =nums.length - 1;
+        while (start + 1 < end){
+            int mid = (start + end)/2;
+            if (nums[mid] > target){
+                end = mid - 1;
+            } else if (nums[mid] < target){
+                start = mid + 1;
+            } else{
+                end = mid;
+            }
+        }
+        if (nums[start] == target){
+            return start;
+        }
+        if (nums[end] == target){
+            return end;
+        }
+        return -1;
+    }
 }
