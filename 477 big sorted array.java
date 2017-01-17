47d730dfd01987e7c67d38e1cbfabdca52b0f81c
AmberGGG <jieyao.gao@gmail.com>
2017-01-17 15:44:57 -0500

    no comments

--- 477 big sorted array.java
+++ 477 big sorted array.java
@@ -1,33 +1,21 @@
-package cs61b;
-import java.io.*;
-import java.util.*;
-public class bigsortedarray{
-	public static int bsa(ArrayReader reader, int target){
-        if (reader == null){
-        	return -1;
-        } 
-		int index = 1;
+public class Solution{
+    public static int findinsort(ArrayReader reader, target k){
+        int index = 1;
         if (reader.get(index - 1) < target){
-        	index = index * 2;
+            index = 2 * index;
         }
-        int start = (index / 2) - 1;
+        int start = index / 2 - 1;
         int end = index - 1;
-        while (start + 1 < end){
-        	int mid = start + (end - start) / 2;
-        	if (reader.get(mid) > target){
-                end = mid - 1;
-        	} else if (reader.get(mid) < target) {
-        		start = mid + 1;
-        	} else {
-        		end = mid;
-        	}
+        while (start < end){
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target){
+                end = mid;
+            } else {
+                start = mid;
+            }
+            if (nums[end] == target){
+                return end;
+            }
+            return -1;
         }
-        if (reader.get(start) == target){
-        	return start;
-        }
-        if (reader.get(end) == target){
-        	return end;
-        }
-        return -1;   
-	}
-}
+    }
